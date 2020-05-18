3b7f6f98ede26cbcbe71a73a84cbecf3ef7ac691
Omar Fasseh <48892906+OmarFasseh@users.noreply.github.com>
2020-05-18 23:18:51 +0200

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e 
 scp -rpi deploy_rsa build Dev@138.91.114.14:~/FrontEndScript
-ssh -i deploy_rsa Dev@138.91.114.14 'sudo -s && nginx -s reload && exit'
+ssh -i deploy_rsa Dev@138.91.114.14 'cd ~/FrontEndScript && ./reload.sh'
 exit
