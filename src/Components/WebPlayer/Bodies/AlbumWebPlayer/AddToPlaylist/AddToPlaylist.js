12b9c5619d8032d89459f30a32c7aa2484f05a70
youmnakhaled <youmnak1@gmail.com>
2020-06-02 10:19:08 +0200

    Create Album edited

--- src/Components/WebPlayer/Bodies/AlbumWebPlayer/AddToPlaylist/AddToPlaylist.js
+++ src/Components/WebPlayer/Bodies/AlbumWebPlayer/AddToPlaylist/AddToPlaylist.js
@@ -136,7 +136,10 @@ render() {
                                             <div className="playlist-index-img-background"  >
                                                 <img src={playlist.imageUrl} alt="Playlist image"
                                                  onError={e => { 
-                                                    e.target.src ='https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX38758453.jpg';}}>  </img>
+                                                    e.target.src ='https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX38758453.jpg';}}> </img>
+                                                {/* <div className="playlist-icon-wrapper"   >
+                                                <i class="fa fa-plus" aria-hidden="true"></i>
+                                                </div> */}
                                                
                                                 <div className="playlist-img-text-wrapper" >               
                                                     <div className="playlist-subtitle"  >
