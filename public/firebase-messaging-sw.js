7f2947bd88ba2a75235fc4a2e371e36bbbb3ab92
youmnakhaled <youmnak1@gmail.com>
2020-06-06 09:26:57 +0200

    notification check

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,57 +1,65 @@
-// importScripts('https://www.gstatic.com/firebasejs/6.6.2/firebase-app.js');
-// importScripts('https://www.gstatic.com/firebasejs/6.6.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.8.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.8.0/firebase-messaging.js');
 
 
-// firebase.initializeApp({
+firebase.initializeApp({
+    apiKey: "AIzaSyDstl21Iann4t-odVPIMFTXpI5ToD1jIC0",
+    authDomain: "totally-not-spotify.firebaseapp.com",
+    databaseURL: "https://totally-not-spotify.firebaseio.com",
+    projectId: "totally-not-spotify",
+    storageBucket: "totally-not-spotify.appspot.com",
+    messagingSenderId: "262598048193",
+    appId: "1:262598048193:web:8eb027331acfe77f625740",
+    measurementId: "G-YDVW3R60NT"
+})
 
-//     messagingSenderId: "262598048193",
-// })
+//const initMessaging=firebase.messaging()
+const messaging = firebase.messaging();
 
-// //const initMessaging=firebase.messaging()
-// const messaging = firebase.messaging();
+messaging.usePublicVapidKey("BKWMGFcg3yIaZ8ONAeIORVydRfg1GFtMnKcCPV-jFyEXWAlbLv8nv9Wtsr4Gu5NsVHZTFl4yD0ZXcZpqsBvrIj8");
 
-// messaging.usePublicVapidKey("BKWMGFcg3yIaZ8ONAeIORVydRfg1GFtMnKcCPV-jFyEXWAlbLv8nv9Wtsr4Gu5NsVHZTFl4yD0ZXcZpqsBvrIj8");
-// // Get Instance ID token. Initially this makes a network call, once retrieved
-// // subsequent calls to getToken will return from cache.
-// // Callback fired if Instance ID token is updated.
-// messaging.onTokenRefresh(() => {
-//     messaging.getToken().then((refreshedToken) => {
-//       console.log('Token refreshed.');
-//       updateUIForPushEnabled(token);
-//       sendSubscriptionToServer(token);
-//       // Indicate that the new Instance ID token has not yet been sent to the
-//       try {
-//         const res = axios.put(this.context.baseURL + "me/notifications/token",
-//         {
-//           "token":token
-//           },
-//            {
-//           headers: {
-//             authorization: "Bearer " + localStorage.getItem("token"),
-//           },
-//         });
-//        if(res.status===204){
-//           console.log("Request Succesful and token is ", token)
-//       }
-//       } 
-//       catch (err) {
-//         console.log(err);
-//       }
-//       // ...
-//     }).catch((err) => {
-//       console.log('Unable to retrieve refreshed token ', err);
-//       showToken('Unable to retrieve refreshed token ', err);
-//     });
-//   });
+// Get Instance ID token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+// Callback fired if Instance ID token is updated.
 
-// /* self.addEventListener('notificationclick', (event) => {
-//     if (event.action) {
-//         clients.openWindow(event.action);
-//     }
-//     event.notification.close();
-// });  */
+messaging.onTokenRefresh(() => {
+    messaging.getToken().then((refreshedToken) => {
+      console.log('Token refreshed.');
+      updateUIForPushEnabled(token);
+      sendSubscriptionToServer(token);
+      // Indicate that the new Instance ID token has not yet been sent to the
+      try {
+        const res = axios.put(this.context.baseURL + "me/notifications/token",
+        {
+          "token":token
+          },
+           {
+          headers: {
+            authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        });
+       if(res.status===204){
+          console.log("Request Succesful and token is ", token)
+      }
+      } 
+      catch (err) {
+        console.log(err);
+      }
+      // ...
+    }).catch((err) => {
+      console.log('Unable to retrieve refreshed token ', err);
+      showToken('Unable to retrieve refreshed token ', err);
+    });
+  });
 
-// // register service worker & handle push events
+/* self.addEventListener('notificationclick', (event) => {
+    if (event.action) {
+        clients.openWindow(event.action);
+    }
+    event.notification.close();
+});  */
+
+// register service worker & handle push events
 // if ('serviceWorker' in navigator) {
 //     window.addEventListener('load', async () => {
 //         const registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js', {
@@ -61,8 +69,11 @@
 //         messaging.onMessage((payload) => {
 //             const title = payload.notification.title;
 //             const options = {
-//                 body: payload.notification.body,
-//                 icon: payload.notification.icon,
+//                 body: payload.data.body,
+//                 icon: payload.data.icon,
+//                 image: payload.data.image,
+//                 click_action: payload.data.click_action,
+//                 data:payload.data.click_action,
 //                 actions: [
 //                     {
 //                         action: payload.fcmOptions.link,
@@ -73,14 +84,13 @@
 //         });
 //     });
 // }
-// self.addEventListener('push', async function(event) {
-//   event.waitUntil(
-//       self.registration.showNotification('title', {
-//         body: 'body'
-//       })
-//   );
-// });
-
+self.addEventListener('push', async function(event) {
+  event.waitUntil(
+      self.registration.showNotification('title', {
+        body: 'body'
+      })
+  );
+});
 // self.addEventListener('notificationclick', function(event) {
 //   console.log('[firebase-messaging-sw.js] Received notificationclick event ', event);
   
@@ -101,23 +111,23 @@
 //       }));
   
 //   });
-//   const showMessage = function(payload){
-//       console.log('showMessage', payload);
-//       const notificationTitle = payload.data.title;
-//       const notificationOptions = {
-//           body: payload.data.body,
-//           icon: payload.data.icon,
-//           image: payload.data.image,
-//           click_action: payload.data.click_action,
-//           data:payload.data.click_action
-//       };  
+  const showMessage = function(payload){
+      console.log('showMessage', payload);
+      const notificationTitle = payload.data.title;
+      const notificationOptions = {
+          body: payload.data.body,
+          icon: payload.data.icon,
+          image: payload.data.image,
+          click_action: payload.data.click_action,
+          data:payload.data.click_action
+      };  
   
   
-//      self.registration.showNotification(notificationTitle,notificationOptions); 
-//   }   
-//   messaging.setBackgroundMessageHandler(showMessage);
-  
-//   self.addEventListener('message', function (evt) {     
-//     console.log("self",self);
-//     showMessage( evt.data );
-//   })
+     self.registration.showNotification(notificationTitle,notificationOptions); 
+  }   
+  messaging.setBackgroundMessageHandler(showMessage);
+  messaging.onMessage(showMessage);
+  self.addEventListener('message', function (evt) {     
+    console.log("self",self);
+    showMessage( evt.data );
+  })
