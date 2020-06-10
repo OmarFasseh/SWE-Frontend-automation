ed3b6c1814e80f1491fbad28e6bb02a717ddd4b8
youmnakhaled <youmnak1@gmail.com>
2020-06-10 15:46:19 +0200

    firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,64 +1,5 @@
 importScripts('https://www.gstatic.com/firebasejs/7.8.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.8.0/firebase-messaging.js');
-/* importScripts('https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.14.4/firebase-messaging.js');
-firebase.initializeApp({
-    messagingSenderId: "262598048193",
-})
-//const initMessaging=firebase.messaging()
-const messaging = firebase.messaging();
-messaging.usePublicVapidKey("BKWMGFcg3yIaZ8ONAeIORVydRfg1GFtMnKcCPV-jFyEXWAlbLv8nv9Wtsr4Gu5NsVHZTFl4yD0ZXcZpqsBvrIj8");
-// Get Instance ID token. Initially this makes a network call, once retrieved
-// subsequent calls to getToken will return from cache.
-// Callback fired if Instance ID token is updated.
-messaging.onTokenRefresh(() => {
-    messaging.getToken().then((refreshedToken) => {
-      console.log('Token refreshed.');
-      // Indicate that the new Instance ID token has not yet been sent to the
-      // app server.
-      setTokenSentToServer(false);
-      // Send Instance ID token to app server.
-      sendTokenToServer(refreshedToken);
-      // ...
-    }).catch((err) => {
-      console.log('Unable to retrieve refreshed token ', err);
-      showToken('Unable to retrieve refreshed token ', err);
-    });
-  });
-self.addEventListener('notificationclick', (event) => {
-    if (event.action) {
-        clients.openWindow(event.action);
-    }
-    event.notification.close();
-}); 
-// register service worker & handle push events
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', async () => {
-        const registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js', {
-            updateViaCache: 'none'
-        });
-        messaging.useServiceWorker(registration);
-        messaging.onMessage((payload) => {
-            const title = payload.notification.title;
-            const options = {
-                body: payload.notification.body,
-              //  icon: payload.notification.icon,
-               // actions: [
-                 //   {
-                   //     action: payload.fcmOptions.link,
-                  //  }
-                //]
-            };
-            registration.showNotification(title, options);           
-        });
-    });
-}  */
-
-
-
-////////////////////////////////
-  
-
 
 
 firebase.initializeApp({
@@ -84,28 +25,27 @@ messaging.usePublicVapidKey("BKWMGFcg3yIaZ8ONAeIORVydRfg1GFtMnKcCPV-jFyEXWAlbLv8
 messaging.onTokenRefresh(() => {
     messaging.getToken().then((token) => {
       console.log('Token refreshed.');
-     // updateUIForPushEnabled(token);
-      //sendSubscriptionToServer(token);
+      updateUIForPushEnabled(token);
+      sendSubscriptionToServer(token);
       // Indicate that the new Instance ID token has not yet been sent to the
-      axios.put("https://totallynotspotify.codes/api/me/notifications/token",
-      {
-        "token":token,
-        "type":"web"
-        },
-         {
-        headers: {
-          authorization: "Bearer " + localStorage.getItem("token"),
-        },
-      })
-      .then(res => {
-        console.log(res)
-     if(res.status===204){
-        console.log("Request Succesful and token is ", token)
-    }
-  })
-  .catch(res => {
-    console.log(res)
-  })
+      try {
+        const res = axios.put(this.context.baseURL + "me/notifications/token",
+        {
+          "token":token,
+          "type":"web"
+          },
+           {
+          headers: {
+            authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        });
+       if(res.status===204){
+          console.log("Request Succesful and token is ", token)
+      }
+      } 
+      catch (err) {
+        console.log(err);
+      }
       // ...
     }).catch((err) => {
       console.log('Unable to retrieve refreshed token ', err);
